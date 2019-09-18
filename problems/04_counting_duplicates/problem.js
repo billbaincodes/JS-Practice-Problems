015b9161dfcda41874efb89577e7ed2c903fc355
bill <williamandrewbain@gmail.com>
2019-09-18 17:59:29 -0600

    add mocha package, remove console logs

--- problems/04_counting_duplicates/problem.js
+++ problems/04_counting_duplicates/problem.js
@@ -2,10 +2,7 @@
 const problem = (str) => {
   // Your code here
   let dupes = 0
-
   letters = str.split('')
-  console.log('letters', letters)
-  
   letters.reduce((accum, char) => {
     let lowerChar = char.toLowerCase();
     if (lowerChar === ' ') { return accum }
@@ -14,15 +11,11 @@ const problem = (str) => {
       return accum
     } else if (accum[lowerChar] === 1) {
       dupes += 1
-      console.log('dupes', dupes)
       accum[lowerChar]++ 
       return accum
     }
-    console.log('accum', accum)
     return accum
   }, {})
-
-  console.log('dupes', dupes)
   return dupes
 }; 
 

89a6b7caa5a190454bdfe0bb0e711584babc9eb1
bill <williamandrewbain@gmail.com>
2019-10-02 21:43:31 -0600

    Add remaining tests for who likes it

--- problems/11_who_likes_it/test.js
+++ problems/11_who_likes_it/test.js
@@ -8,5 +8,26 @@ describe('Who likes it?', () => {
     chai.expect(result).to.equal("No one likes this")
   });
 
+  it('Displays one like', () => {
+    let result = problem(['Peter']);
+    chai.expect(result).to.equal("Peter likes this")
+  });
+
+  it('Displays two likes', () => {
+    let result = problem(["Jacob", "Alex"]);
+    chai.expect(result).to.equal("Jacob and Alex like this")
+  });
+
+  it('Displays three likes', () => {
+    let result = problem(["Max", "John", "Mark"]);
+    chai.expect(result).to.equal("Max, John and Mark like this")
+  });
+
+  it('Displays four or more likes', () => {
+    let result = problem(["Alex", "Jacob", "Mark", "Max"]);
+    chai.expect(result).to.equal("Alex, Jacob and 2 others like this")
+  });
+
+
 });
 

1e867888f02af7e86a5ecc4fb46a864e18f870e0
bill <williamandrewbain@gmail.com>
2019-10-02 21:42:49 -0600

    solve like system problem

--- problems/11_who_likes_it/problem.js
+++ problems/11_who_likes_it/problem.js
@@ -4,6 +4,14 @@ const problem = (arr) => {
 
   if(!arr.length) {
     return 'No one likes this'
+  } else if (arr.length === 1) {
+    return `${arr[0]} likes this`
+  } else if (arr.length === 2) {
+    return `${arr[0]} and ${arr[1]} like this`
+  } else if (arr.length === 3) {
+    return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`
+  } else {
+    return `${arr[0]}, ${arr[1]} and ${arr.length-2} others like this`
   }
 
 }

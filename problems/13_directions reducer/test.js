3f8019df421dee784da53975898c86075dea08ab
bill <williamandrewbain@gmail.com>
2019-10-06 10:04:26 -0600

    add more tests to directions reducer

--- problems/13_directions reducer/test.js
+++ problems/13_directions reducer/test.js
@@ -1,22 +1,61 @@
-const chai = require('chai');
-const problem = require('./problem');
+const chai = require("chai");
+const problem = require("./problem");
 
-describe('Directions Reduction', () => {
-
-  it('Cancels out usesless directions', () => {
+describe("Directions Reduction", () => {
+  it("Cancels out usesless directions", () => {
     let result = problem(["NORTH", "SOUTH", "EAST", "WEST"]);
-    chai.expect(result).to.deep.equal([])
+    chai.expect(result).to.deep.equal([]);
   });
 
-  it('Doesnt reduce valid directions', () => {
+  it("Doesnt reduce valid directions", () => {
     let result = problem(["NORTH", "WEST", "SOUTH", "EAST"]);
-    chai.expect(result).to.deep.equal(["NORTH", "WEST", "SOUTH", "EAST"])
+    chai.expect(result).to.deep.equal(["NORTH", "WEST", "SOUTH", "EAST"]);
   });
 
-  it('Does both correctly', () => {
-    let result = problem(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]);
-    chai.expect(result).to.deep.equal(["WEST"])
+  it("Does both correctly, ex1", () => {
+    let result = problem([
+      "NORTH",
+      "SOUTH",
+      "SOUTH",
+      "EAST",
+      "WEST",
+      "NORTH",
+      "WEST"
+    ]);
+    chai.expect(result).to.deep.equal(["WEST"]);
   });
+
+  it("Does both correctly, ex2", () => {
+    let result = problem([
+      "EAST",
+      "EAST",
+      "WEST",
+      "NORTH",
+      "WEST",
+      "EAST",
+      "EAST",
+      "SOUTH",
+      "NORTH",
+      "WEST"
+    ]);
+    chai.expect(result).to.deep.equal(["EAST", "NORTH"]);
+  });
+
+  it("Does both correctly, ex3", () => {
+    let result = problem([
+    'EAST',
+    'WEST',
+    'SOUTH',
+    'NORTH',
+    'EAST',
+    'SOUTH',
+    'EAST',
+    'WEST',
+    'SOUTH',
+    'NORTH']);
+    chai.expect(result).to.deep.equal(["EAST", "SOUTH"]);
+  });
+
+
 
 });
-

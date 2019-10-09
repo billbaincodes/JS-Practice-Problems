10753a78fb5980ef41a6efcc4f68833d29acd93b
bill <williamandrewbain@gmail.com>
2019-10-08 19:03:38 -0600

    Add Pete the baker problem

---
+++ problems/14_pete_the_baker/test.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const problem = require('./problem');
+
+describe('problem name', () => {
+  it('should pass', () => {
+    let result = problem();
+    chai.expect(result).to
+  });
+});
+

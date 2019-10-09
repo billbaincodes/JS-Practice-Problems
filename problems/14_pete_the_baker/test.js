5b681995feec4c0aaf3a84aa18d698d7f18c7d1e
bill <williamandrewbain@gmail.com>
2019-10-09 17:54:53 -0600

    fix baker prompt typos, write baker tests

--- problems/14_pete_the_baker/test.js
+++ problems/14_pete_the_baker/test.js
@@ -1,10 +1,23 @@
 const chai = require('chai');
 const problem = require('./problem');
 
-describe('problem name', () => {
-  it('should pass', () => {
-    let result = problem();
-    chai.expect(result).to
+describe('Pete the Baker', () => {
+
+  it('Returns more cakes when more ingredients are available', () => {
+    let result = problem(
+      { flour: 500, sugar: 200, eggs: 1 }, 
+      { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }
+    );
+    chai.expect(result).to.equal(2)
   });
+
+  it('Returns 0 when not enough ingredients are available.', () => {
+    let result = problem(
+      {apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100},
+      {sugar: 500, flour: 2000, milk: 2000}
+    );
+    chai.expect(result).to.equal(0)
+  });
+
 });
 

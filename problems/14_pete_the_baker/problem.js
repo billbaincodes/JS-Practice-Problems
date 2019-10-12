8941e4cc027f2e6f4c36b19f13e618b3c147c39b
bill <williamandrewbain@gmail.com>
2019-10-11 22:47:40 -0600

    baker problem refactor

--- problems/14_pete_the_baker/problem.js
+++ problems/14_pete_the_baker/problem.js
@@ -1,23 +1,39 @@
 
 const problem = (recipe, ingredients) => {
   // Your code here
-  let ingredientList = []
+  let ingredientList = Object.keys(recipe)
   let availability = []
 
-  for (let item in recipe) {
-    ingredientList.push(item);
-  }
-
   for (let i = 0; i < ingredientList.length; i++) {
-    let required = recipe[ingredientList[i]]
-    let available = ingredients[ingredientList[i]]
-    if (available < required || available === undefined) {
+    let res = ingredients[ingredientList[i]] / recipe[ingredientList[i]]
+    if (isNaN(res)){
       return 0
     } else {
-      availability.push(Math.floor(available / required))
+      availability.push(Math.floor(res))
     }
   }
   return availability.sort(function(a, b){return a-b})[0]
 }
 
-module.exports = problem;
+module.exports = problem
+
+
+
+// ROUND I
+// let ingredientList = []
+// let availability = []
+
+// for (let item in recipe) {
+//   ingredientList.push(item);
+// }
+
+// for (let i = 0; i < ingredientList.length; i++) {
+//   let required = recipe[ingredientList[i]]
+//   let available = ingredients[ingredientList[i]]
+//   if (available < required || available === undefined) {
+//     return 0
+//   } else {
+//     availability.push(Math.floor(available / required))
+//   }
+// }
+// return availability.sort(function(a, b){return a-b})[0]

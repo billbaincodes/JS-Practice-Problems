7d45c9bd59017e9e4941cdf1531f7d80758bac5c
bill <williamandrewbain@gmail.com>
2019-10-15 18:23:14 -0600

    solve ip converter, add more test cases

--- problems/15_ip_converter/problem.js
+++ problems/15_ip_converter/problem.js
@@ -1,10 +1,11 @@
 
 const problem = (ip) => {
   // Your code here
-
-  let prettyIp = ip.split('.').map(num => Number(num).toString(2))
-  return prettyIp
-
+  let bigBin = ip.split('.').map(num => Number(num).toString(2)).map(bin => {
+    for (let i = 0; bin.length < 8; i++){ bin = `0${bin}` }
+    return bin
+  }).join('')
+  return parseInt(bigBin, 2)
 }
 
 module.exports = problem;
--- problems/15_ip_converter/test.js
+++ problems/15_ip_converter/test.js
@@ -8,5 +8,15 @@ describe('IP Converter', () => {
     chai.expect(result).to.equal(3232235777)
   });
 
+  it('Converts 10.0.0.0', () => {
+    let result = problem('10.0.0.0');
+    chai.expect(result).to.equal(167772160)
+  });
+
+  it('Converts 176.16.0.1', () => {
+    let result = problem('176.16.0.1');
+    chai.expect(result).to.equal(2953838593)
+  });
+
 });
 

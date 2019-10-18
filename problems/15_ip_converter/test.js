7e55e1b8055e27650b039f32f2b177d4e590d82c
bill <williamandrewbain@gmail.com>
2019-10-17 22:11:06 -0600

    add more tests for prob 15B

--- problems/15_ip_converter/test.js
+++ problems/15_ip_converter/test.js
@@ -18,9 +18,21 @@ describe('IP Converter', () => {
     chai.expect(result).to.equal(2953838593)
   });
 
+
+
   it('Converts 3232235777', () => {
     let result = problemB(3232235777);
     chai.expect(result).to.deep.equal('192.168.1.1')
+  });
+
+  it('Converts 167772160', () => {
+    let result = problemB(167772160);
+    chai.expect(result).to.deep.equal('10.0.0.0')
+  });
+
+  it('Converts 2953838593', () => {
+    let result = problemB(2953838593);
+    chai.expect(result).to.deep.equal('176.16.0.1')
   });
 
   

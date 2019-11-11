c06fd3de4bcebbe384702bd3e4fab7d9d5d04a0d
bill <williamandrewbain@gmail.com>
2019-11-10 19:19:16 -0700

    solve integer range, fix missing .deep in tests

--- problems/18_generate_integer_range/problem.js
+++ problems/18_generate_integer_range/problem.js
@@ -1,7 +1,9 @@
 
-const problem = () => {
+const problem = (min, max, step) => {
   // Your code here
-
+  let result = [];
+  for (let i = min; i <= max; i += step) { result.push(i) };
+  return result;
 }
 
 module.exports = problem;

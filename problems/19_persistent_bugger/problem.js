29af1e0b80c2121c7cff543c3d6e57505fa9880e
bill <williamandrewbain@gmail.com>
2019-11-13 17:56:17 -0700

    Solve persistent bugger 🐛

--- problems/19_persistent_bugger/problem.js
+++ problems/19_persistent_bugger/problem.js
@@ -1,7 +1,23 @@
 
 const problem = (num) => {
   // Your code here
-  return num
+  let counter = 1
+  num = num.toString().split('')
+
+  if (num.length === 1) { return 0 }
+  multiplier = (num) => {
+    const product = num.reduce((prod, curr) => {
+      return prod * curr
+    }, 1)
+    if (product.toString().length >= 2) {
+      counter++
+      multiplier(product.toString().split(''))
+    } 
+    return product
+  }
+
+  multiplier(num)
+  return counter
 }
 
 module.exports = problem;

bf25a4b131ca1de24b9f89ba80f3277e356f6db7
bill <williamandrewbain@gmail.com>
2019-11-15 19:56:11 -0700

    add duplicate encoder problem

---
+++ problems/20_duplicate_encoder/test.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const problem = require('./problem');
+
+describe('problem name', () => {
+
+  it('should pass', () => {
+    let result = problem();
+    chai.expect(result).to
+  });
+
+});
+

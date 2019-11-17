c3c9192adf78289d03baff61d1a2e0ec90daa33a
bill <williamandrewbain@gmail.com>
2019-11-16 22:42:52 -0700

    start dupe encoder solution, add more tests

--- problems/20_duplicate_encoder/problem.js
+++ problems/20_duplicate_encoder/problem.js
@@ -1,6 +1,27 @@
 
-const problem = () => {
+const problem = (str) => {
   // Your code here
+
+  arr = str.split("")
+
+  let dupes = arr.reduce((accum, curr) => {
+
+    if (accum.curr){
+      accum.curr++
+    } else {
+      accum.curr = 1
+    }
+    return accum
+
+  }, {})
+
+  return arr.map((letter) => {
+    if (dupes.letter) {
+      return ')'
+    } else {
+      return '('
+    }
+  }).join('')
 
 }
 
--- problems/20_duplicate_encoder/test.js
+++ problems/20_duplicate_encoder/test.js
@@ -3,9 +3,24 @@ const problem = require('./problem');
 
 describe('problem name', () => {
 
-  it('should pass', () => {
-    let result = problem();
-    chai.expect(result).to
+  it('Encodes din', () => {
+    let result = problem('din');
+    chai.expect(result).to.equal('(((')
+  });
+
+  it('Encodes recede', () => {
+    let result = problem('recede');
+    chai.expect(result).to.equal('()()()')
+  });
+
+  it('Encodes Success', () => {
+    let result = problem('Success');
+    chai.expect(result).to.equal(')())())')
+  });
+
+  it('Encodes (( @', () => {
+    let result = problem('(( @');
+    chai.expect(result).to.equal('))((')
   });
 
 });

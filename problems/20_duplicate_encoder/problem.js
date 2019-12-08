b260e886cb90c60119b99e437cadcee4688df1df
bill <williamandrewbain@gmail.com>
2019-12-07 21:51:03 -0700

    start alternate dupe encoder solution

--- problems/20_duplicate_encoder/problem.js
+++ problems/20_duplicate_encoder/problem.js
@@ -3,23 +3,33 @@ const problem = (str) => {
   // Your code here
   arr = str.split("")
 
+  return arr.map((letter, i) => {
+
+    console.log({ letter, i })
+
+    return arr.forEach((l2, i2) => {
+      if (i2 !== i) {
+        if (letter.toLowerCase() == l2.toLowerCase()) {
+          console.log({ letter, l2})
+          return ')'
+        }
+      }
+    })
+
+
+  }).join('')
+
+
+
   let dupes = arr.reduce((accum, curr) => {
     let lower = curr.toLowerCase()
-    if (accum[lower]){
-      accum[lower]++
-    } else {
-      accum[lower] = 1
-    }
+    accum[lower] ? accum[lower]++ : accum[lower] = 1
     return accum
   }, {})
 
   return arr.map((letter) => {
     let check = dupes[letter.toLowerCase()]
-    if (check > 1) {
-      return ')'
-    } else {
-      return '('
-    }
+    return check > 1 ? ')' : '(' 
   }).join('')
 
 }

640c2d4a98a998ee38fe1d633917cb6d4eef78a5
bill <williamandrewbain@gmail.com>
2020-02-11 21:11:14 -0700

    pass string incrementer tests

--- problems/22_string_incrementer/problem.js
+++ problems/22_string_incrementer/problem.js
@@ -1,7 +1,20 @@
 
 const problem = (str) => {
   // Your code here
-  return str + '1'
+  let zeroes = str.match(/\d+$/)
+  let match = str.match(/[1-9]+$/)
+
+  if (!match) {
+    return `${str}1`
+  } else {
+    let newVal = Number(match[0]) + 1
+    if (String(newVal).length > match[0].length) {
+      let letters = str.substring(0, str.length - zeroes[0].length)
+      return letters + newVal
+    } else {
+      return str.replace(match[0], newVal)
+    }
+  }
 }
 
 module.exports = problem;

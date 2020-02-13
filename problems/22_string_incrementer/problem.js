a6348e1900c7f985ba6b59479774d02e73167535
bill <williamandrewbain@gmail.com>
2020-02-12 21:40:52 -0700

    improve string incrementer test cases, start refactor

--- problems/22_string_incrementer/problem.js
+++ problems/22_string_incrementer/problem.js
@@ -2,19 +2,28 @@
 const problem = (str) => {
   // Your code here
   let zeroes = str.match(/\d+$/)
-  let match = str.match(/[1-9]+$/)
+  let match = str.match(/[0-9]+$/)
 
-  if (!match) {
-    return `${str}1`
-  } else {
-    let newVal = Number(match[0]) + 1
-    if (String(newVal).length > match[0].length) {
-      let letters = str.substring(0, str.length - zeroes[0].length)
-      return letters + newVal
-    } else {
-      return str.replace(match[0], newVal)
-    }
-  }
+  if (!match) { return `${str}1` }
+
+  let newVal = Number(match[0]) + 1
+  let letters = str.substring(0, str.length - match[0].length)
+
+  return `${letters}${newVal}`
 }
 
 module.exports = problem;
+
+
+
+// if (!zeroes) {
+//   return `${str}1`
+// } else {
+//   let newVal = Number(match[0]) + 1
+//   if (String(newVal).length > match[0].length) {
+//     let letters = str.substring(0, str.length - zeroes[0].length)
+//     return letters + newVal
+//   } else {
+//     return str.replace(match[0], newVal)
+//   }
+// }
--- problems/22_string_incrementer/test.js
+++ problems/22_string_incrementer/test.js
@@ -2,17 +2,17 @@ const chai = require('chai');
 const problem = require('./problem');
 
 describe('string incrementer', () => {
-  it('should start counting at 1 if no number present', () => {
+  it('Start at one: foo -> foo1', () => {
     let result = problem('foo');
     chai.expect(result).to.equal('foo1')
   });
 
-  it('should add to number if present', () => {
+  it('Increment: foobar23 -> foobar24', () => {
     let result = problem('foobar23');
     chai.expect(result).to.equal('foobar24')
   });
 
-  it('ignore leading zeros', () => {
+  it('Ignore zeroes: foo0042 -> foo0043', () => {
     let result = problem('foo0042');
     chai.expect(result).to.equal('foo0043')
   });
@@ -21,5 +21,13 @@ describe('string incrementer', () => {
     let result = problem('foo099');
     chai.expect(result).to.equal('foo100')
   });
+
+  it('should do it all', () => {
+    let result = problem('foobar000');
+    chai.expect(result).to.equal('foobar001')
+
+    let result2 = problem('foo0099');
+    chai.expect(result2).to.equal('foo0100')
+  })
 });
 

42e1ad643aefeaf14c1b9f3a4f61d344184654db
bill <williamandrewbain@gmail.com>
2020-02-15 09:52:16 -0700

    add time formatter test cases

--- problems/23_time_formatter/test.js
+++ problems/23_time_formatter/test.js
@@ -1,10 +1,30 @@
 const chai = require('chai');
 const problem = require('./problem');
 
-describe('problem name', () => {
-  it('should pass', () => {
-    let result = problem();
-    chai.expect(result).to
+describe('time formatter', () => {
+  it('should handle seconds', () => {
+    let result = problem(1);
+    chai.expect(result).to.equal('1 second')
+  });
+
+  it('should handle minutes', () => {
+    let result = problem(62);
+    chai.expect(result).to.equal('1 minute and 2 seconds')
+  });
+
+  it('should use one unit of time when possible ', () => {
+    let result = problem(62);
+    chai.expect(result).to.equal('2 minutes')
+  });
+
+  it('should handle hours ', () => {
+    let result = problem(3600);
+    chai.expect(result).to.equal('1 hour')
+  });
+
+  it('advanced cases', () => {
+    let result = problem(3662);
+    chai.expect(result).to.equal('1 hour, 1 minute and 2 seconds')
   });
 });
 

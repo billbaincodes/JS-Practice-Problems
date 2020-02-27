d8a299696af85e6e5610108fe2f846035b9e1dd9
bill <williamandrewbain@gmail.com>
2020-02-26 21:38:29 -0700

    Start time formatter problem

--- problems/23_time_formatter/problem.js
+++ problems/23_time_formatter/problem.js
@@ -1,7 +1,19 @@
 
-const problem = () => {
+const problem = (time) => {
   // Your code here
+  if (time === 0) { return 'now' }
 
+  //Minutes
+  if (time > 60) {
+    let seconds = time%60
+    let minutes = Math.floor(time/60)
+    return `${minutes} minute and ${seconds} ${seconds > 1 ? 'seconds' : 'second'}`
+  }
+
+  return `${time} ${time > 1 ? 'seconds' : 'second'}`
 }
 
 module.exports = problem;
+
+// 365 days / 24 hours / 60 minutes / 60 seconds
+// 31536000   86400      3600         60

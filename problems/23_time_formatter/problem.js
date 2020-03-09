dd93ff26df41f7dee109cdcb2d4bc01b106308f6
bill <williamandrewbain@gmail.com>
2020-03-08 19:13:33 -0600

    Refactor, make print generation programmatic

--- problems/23_time_formatter/problem.js
+++ problems/23_time_formatter/problem.js
@@ -1,29 +1,21 @@
 const problem = time => {
   // Your code here
-  if (time === 0) {
-    return "now";
-  }
+  if (time === 0) { return "now"; }
+  let clock = {};
+  let print = [];
 
-  let hours = Math.floor(time / 3600);
+  clock.hour = Math.floor(time / 3600);
   let hRemainder = time % 3600;
-  let minutes = Math.floor(hRemainder / 60);
-  let seconds = hRemainder % 60;
+  clock.minute = Math.floor(hRemainder / 60);
+  clock.second = hRemainder % 60;
 
-  hourPrint = hours ? (hours > 1 ? `${hours} hours` : `${hours} hour`) : null;
-  minutePrint = minutes
-    ? minutes > 1
-      ? `${minutes} minutes`
-      : `${minutes} minute`
-    : null;
-  secondPrint = seconds
-    ? seconds > 1
-      ? `${seconds} seconds`
-      : `${seconds} second`
-    : null;
-
-  let print = [hourPrint, minutePrint, secondPrint];
-
-  print = print.filter(val => val);
+  for (key in clock) {
+    if (clock[key] > 1){
+      print.push(`${clock[key]} ${key}s`)
+    } else if (clock[key] === 1) {
+      print.push(`${clock[key]} ${key}`)
+    }
+  }
 
   if (print.length > 1) {
     print = print.map((unit, idx) => {
@@ -35,7 +27,6 @@ const problem = time => {
         return `${unit},`;
       }
     });
-
     return print.join(" ");
   } else {
     return print[0];

69bc88119bd62d5a67ee44dc60b641fc271235b6
bill <williamandrewbain@gmail.com>
2020-03-11 21:32:48 -0600

    expand time formatter tests for days and years, improve time formatter

--- problems/23_time_formatter/problem.js
+++ problems/23_time_formatter/problem.js
@@ -3,9 +3,14 @@ const problem = time => {
   if (time === 0) { return "now"; }
   let clock = {};
   let print = [];
+  let yRemainder, dRemainder, hRemainder;
 
-  clock.hour = Math.floor(time / 3600);
-  let hRemainder = time % 3600;
+  clock.year = Math.floor(time / 31536000)
+  yRemainder = time % 31536000
+  clock.day = Math.floor(yRemainder / 86400)
+  dRemainder = time % 86400
+  clock.hour = Math.floor(dRemainder / 3600);
+  hRemainder = time % 3600;
   clock.minute = Math.floor(hRemainder / 60);
   clock.second = hRemainder % 60;
 
@@ -17,20 +22,17 @@ const problem = time => {
     }
   }
 
-  if (print.length > 1) {
-    print = print.map((unit, idx) => {
-      if (idx === print.length - 2) {
-        return unit;
-      } else if (idx === print.length - 1) {
-        return `and ${unit}`;
-      } else {
-        return `${unit},`;
-      }
-    });
-    return print.join(" ");
-  } else {
-    return print[0];
-  }
+  return print.map((unit, idx) => {
+    if (print.length === 1) { 
+      return unit; 
+    } else if (idx === print.length - 2) {
+      return unit;
+    } else if (idx === print.length - 1) {
+      return `and ${unit}`;
+    } else {
+      return `${unit},`;
+    }
+  }).join(" ");
 };
 
 module.exports = problem;
--- problems/23_time_formatter/test.js
+++ problems/23_time_formatter/test.js
@@ -22,9 +22,19 @@ describe('time formatter', () => {
     chai.expect(result).to.equal('2 minutes')
   });
 
-  it('should handle hours ', () => {
+  it('should handle hours', () => {
     let result = problem(3600);
     chai.expect(result).to.equal('1 hour')
+  });
+
+  it('should handle days', () => {
+    let result = problem(15731080);
+    chai.expect(result).to.equal('182 days, 1 hour, 44 minutes and 40 seconds')
+  });
+
+  it('should handle years', () => {
+    let result = problem(253374061);
+    chai.expect(result).to.equal('8 years, 12 days, 13 hours, 41 minutes and 1 second')
   });
 
   it('advanced cases', () => {

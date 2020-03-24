6ad8e62b04edbc1800ed79370049067ff9c3e3f0
bill <williamandrewbain@gmail.com>
2020-03-23 21:42:29 -0600

    Start fib product refactor

--- problems/24_fibonacci_products/problem.js
+++ problems/24_fibonacci_products/problem.js
@@ -1,26 +1,26 @@
 
 const problem = (prod) => {
   // Your code here
-  let fibSet = [0, 1];
-  let idx = 2;
-  let fibProd = 0;
-
-  while(fibProd < prod) {
-    fibProd = fibSet[idx - 1] *  fibSet[idx - 2];
-    if (fibProd === prod) { 
+  let fib1 = 0
+  let fib2 = 1
+  // let fibProd = 0;
+  while(fib1 * fib2 < prod) {
+    // fibProd = fib1 *  fib2;
+    if (fib1 * fib2 === prod) { 
       return [
-        fibSet[idx - 2],
-        fibSet[idx - 1],
+        fib1,
+        fib2,
         true,
       ]
     }
-    fibSet.push(fibSet[idx - 1] + fibSet[idx - 2]);
-    idx++
+    nextFib = fib1 + fib2
+    fib1 = fib2
+    fib2 = nextFib
   };
 
   return [
-    fibSet[fibSet.length - 3],
-    fibSet[fibSet.length - 2],
+    fib2-fib1,
+    fib1,
     false,
   ];
 }

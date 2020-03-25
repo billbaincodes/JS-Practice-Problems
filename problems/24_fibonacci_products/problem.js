c97981850426f6095b68f2f8c7983a19acbf0a55
bill <williamandrewbain@gmail.com>
2020-03-24 21:30:23 -0600

    tighten up fib answer

--- problems/24_fibonacci_products/problem.js
+++ problems/24_fibonacci_products/problem.js
@@ -3,26 +3,15 @@ const problem = (prod) => {
   // Your code here
   let fib1 = 0
   let fib2 = 1
-  // let fibProd = 0;
-  while(fib1 * fib2 < prod) {
-    // fibProd = fib1 *  fib2;
+  while (fib1 * fib2 <= prod) {
     if (fib1 * fib2 === prod) { 
-      return [
-        fib1,
-        fib2,
-        true,
-      ]
+      return [ fib1, fib2, true ]
     }
     nextFib = fib1 + fib2
     fib1 = fib2
     fib2 = nextFib
   };
-
-  return [
-    fib2-fib1,
-    fib1,
-    false,
-  ];
+  return [ fib1, fib2, false ];
 }
 
 module.exports = problem;

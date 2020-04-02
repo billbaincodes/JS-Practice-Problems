9d21f99f5ee4cde3b28b3ba08c87a633f33ccf08
bill <williamandrewbain@gmail.com>
2020-04-01 21:45:13 -0600

    start next bigger number problem / permutation

--- problems/25_next_bigger_number/problem.js
+++ problems/25_next_bigger_number/problem.js
@@ -2,6 +2,39 @@
 const problem = (num) => {
   // Your code here
   if (num < 10) { return -1 }
+
+  let arr = `${num}`.split('')
+  let rounds = arr.length;
+
+  let result = []
+  for (let i = 0; i < rounds; i++) {
+    let perm = []
+    perm.push(arr[i])
+
+    let j = i;
+    while( arr[j+1] ) {
+      console.log('+', arr[j+1])
+      perm.push(arr[j+1])
+      j++
+    }
+    j = i
+    while( arr[j-1] ) {
+      console.log('-', arr[j-1])
+      perm.push(arr[j-1])
+      j--
+    }
+    result.push(Number(perm.join('')))
+  }
+
+  console.log({ result })
+
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] > num) {
+      return result[i]
+    }
+  }
+
+  return -1
 }
 
 module.exports = problem;

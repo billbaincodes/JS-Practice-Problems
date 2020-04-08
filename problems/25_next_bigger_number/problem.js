fe249ef47243634625c0e71fa780f958e5117a9d
bill <williamandrewbain@gmail.com>
2020-04-07 21:22:20 -0600

    Change while loops to nested fors

--- problems/25_next_bigger_number/problem.js
+++ problems/25_next_bigger_number/problem.js
@@ -8,26 +8,15 @@ const problem = (num) => {
 
   let result = []
   for (let i = 0; i < rounds; i++) {
-    let perm = []
-    perm.push(arr[i])
-
-    let j = i;
-    while( arr[j+1] ) {
-      console.log('+', arr[j+1])
-      perm.push(arr[j+1])
-      j++
+    for (let j = 0; j < rounds; j++) {
+      let currArr = `${num}`.split('');
+      console.log({currArr})
+      currArr[i] = currArr[j];
+      result.push(currArr)
     }
-    j = i
-    while( arr[j-1] ) {
-      console.log('-', arr[j-1])
-      perm.push(arr[j-1])
-      j--
-    }
-    result.push(Number(perm.join('')))
   }
 
   console.log({ result })
-
   for (let i = 0; i < result.length; i++) {
     if (result[i] > num) {
       return result[i]

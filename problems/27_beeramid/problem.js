621715093d549c8ef522d81f2684105fe1371270
bill <williamandrewbain@gmail.com>
2021-03-08 12:34:03 -0700

    solve beeramid 🍻

--- problems/27_beeramid/problem.js
+++ problems/27_beeramid/problem.js
@@ -1,7 +1,20 @@
 
 const problem = (bonus, price) => {
   // Your code here
+  let beeramid = 0;
+  let wallet = bonus;
 
+  solver = (price, level) => {
+    let cost = price * (level**2);
+    wallet -= cost;
+    if (wallet >= 0) {
+      beeramid += 1;
+      solver(price, level + 1);
+    }
+    return beeramid;
+  }
+
+  return solver(price, 1);
 }
 
 module.exports = problem;
